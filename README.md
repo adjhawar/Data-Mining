54eb4047d7e2e028ed46634e0635ee50666b5543
adjhawar <adjhawar0697@gmail.com>
2018-11-11 00:37:18 +0530

    Some changes

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-s Data-Mining
-SHIIIT
-
-<<<<<<< HEAD
-Th
-=======
-
-9172d1ac752bee4339ecda6e07b7d32ea67e2e56
->>>>>>> 40260b680041d496ead7993840d88b9c2f70585d
+The rows containing null values were removed.
--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
 
-#removing the 
+#removing the null rows
 f="responses.csv"
 df=pd.read_csv(f)
 df1=df.dropna(axis=0)
-df1.to_csv("non_null_responses.csv")
+df1.to_csv("non_null_responses.csv",index=False)
 
+print(df1.corr())
