84cc8dfa27389f696d6876a55c9f2d0e9530a0cc
adjhawar <adjhawar0697@gmail.com>
2018-11-11 03:47:02 +0530

    The String values are mapped

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 The rows containing null values were removed.
+
+The some data values contain String values. Those values were mapped to integers as:
+1. Smoking {'never smoked': 0, 'tried smoking': 1, 'current smoker': 2, 'former smoker': 3}
+2. Alcohol {'drink a lot': 2, 'social drinker': 1, 'never': 0}
+3. Punctuality {'i am always on time': 0, 'i am often early': 1, 'i am often running late': 2}
+4. Lying {'never': 0, 'sometimes': 1, 'everytime it suits me': 2, 'only to avoid hurting someone': 3}
+5. Internet usage {'few hours a day': 0, 'less than an hour a day': 1, 'most of the day': 2}
+6. Gender {'female': 0, 'male': 1}
+7. Left - right handed {'right handed': 0, 'left handed': 1}
+8. Education {'college/bachelor degree': 3, 'secondary school': 2, 'primary school': 1, 'masters degree': 4, 'doctorate degree': 5, 'currently a primary school pupil': 0}
+9. Only child {'no': 0, 'yes': 1}
+10. Village - town {'village': 0, 'city': 1}
+11. House - block of flats {'block of flats': 0, 'house/bungalow': 1}
+
--- preprocess.py
+++ preprocess.py
@@ -2,9 +2,38 @@ import pandas as pd
 import numpy as np
 
 #removing the null rows
-f="responses.csv"
-df=pd.read_csv(f)
-df1=df.dropna(axis=0)
-df1.to_csv("non_null_responses.csv",index=False)
+def remove_null():	
+	f="responses.csv"
+	df=pd.read_csv(f)
+	df1=df.dropna(axis=0)
+	df1.to_csv("non_null_responses.csv",index=False)
 
-print(df1.corr())
+def map_values():
+	f="replaced_responses.csv"
+	df=pd.read_csv("responses.csv")
+	l=df.select_dtypes(include=['object']).columns
+	for x in l:
+		unique=df[x].unique()
+		if x=="Alcohol":
+			l=[2,1,0]
+		elif x=="Education":
+			l=[3,2,1,4,5,0]
+		else:
+			l=[i for i in range(len(unique))]
+		dic=dict(zip(unique,l))
+		df[x]=df[x].replace(dic)
+	df.to_csv(f,index=False)
+
+map_values()
+remove_null()
+'''
+c=df1.corr().abs()
+s = c.unstack()
+so = s.sort_values(kind="quicksort")
+values = [0 if i<0.5 else i for i in so ]
+m1=list(filter(lambda a:a !=0,values))
+values = [0 if i==1.0 else i for i in m1]
+m2=list(filter(lambda a:a !=0,values))
+print(len(m1))
+print(len(m2))
+print(so[-len(m1):len(m2)-len(m1)])'''
